9736c43b727fa6383cf0dfa2780ede06154cb1d1
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2024-08-27 01:36:36 +0200

    created the structure of the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,23 @@
+import { appleImg, bagImg, searchImg } from '../utils';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt='apple' width={14} height={18} />
+
+        <div>
+          {['phones', 'Macbooks', 'Tablets'].map((nav, i) => (
+            <div key={i}>{nav}</div>
+          ))}
+        </div>
+        <div>
+          <img src={searchImg} alt='search' width={18} height={18} />
+          <img src={bagImg} alt='bag' width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
