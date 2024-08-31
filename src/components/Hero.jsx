bf7f7c1243a8efbb0a32c636d57c700c5874a809
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2024-08-31 13:35:37 +0200

    added animation to hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,19 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  useGSAP(() => {
+    gsap.to('#hero', { opacity: 1 });
+  }, []);
+  return (
+    <section className='w-full nav-height bg-black relative'>
+      <div className='h-5/6 w-full flex-center flex-col'>
+        <p id='hero' className='hero-title'>
+          iPhone 15 Pro
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
