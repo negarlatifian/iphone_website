4260a1298d6a4ac99c30b3ece6eaa116d673d561
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2024-09-01 21:23:24 +0200

    added the video hero and window resize function

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,46 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import { useEffect, useState } from 'react';
+import { heroVideo, smallHeroVideo } from '../utils';
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrcSet);
+    return () => {
+      window.removeEventListener('resize', handleVideoSrcSet);
+    };
+  }, []);
   useGSAP(() => {
-    gsap.to('#hero', { opacity: 1 });
+    gsap.to('#hero', { opacity: 1, delay: 1.5 });
   }, []);
   return (
     <section className='w-full nav-height bg-black relative'>
-      <div className='h-5/6 w-full flex-center flex-col'>
+      <div className='h-5/6 w-full flex flex-center flex-col'>
         <p id='hero' className='hero-title'>
           iPhone 15 Pro
         </p>
+        <div className='md:w-10/12 w-9/12 contents '>
+          <video
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+            className='pointer-events-none'
+          >
+            <source src={videoSrc} type='video/mp4' />
+          </video>
+        </div>
       </div>
     </section>
   );
